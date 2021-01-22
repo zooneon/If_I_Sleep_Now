665eef497bc898e569cd61a8aeb1e1a3245cf3f2
hand <tkdwns0301@gmail.com>
2021-01-22 21:42:03 +0900

    [Init] Add Project file

---
+++ I2SN/I2SN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I2SN
+//
+//  Created by 손상준 on 2021/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
