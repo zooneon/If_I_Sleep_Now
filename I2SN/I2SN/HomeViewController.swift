94163b79361ebbe2562838c71aa5789f25665f85
hand <tkdwns0301@gmail.com>
2021-01-25 00:01:07 +0900

    [Add] RemainTime Calculater
    - 남은 시간 HH:mm:ss 계산해서 출력
    - 카운트 다운 구현

--- I2SN/I2SN/HomeViewController.swift
+++ I2SN/I2SN/HomeViewController.swift
@@ -8,15 +8,24 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    var alarmTime : Date?
+    var btnStartFlag = true
+    
+    let timeSelector: Selector = #selector(HomeViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var startButton: UIButton!
+    @IBOutlet var lblRemainTime: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         assignBackground()
         setNavigationBar()
         setDatePicker()
+        
+        lblRemainTime.isHidden = true
     }
     
     func assignBackground(){
@@ -42,5 +51,91 @@ class HomeViewController: UIViewController {
     func setDatePicker() {
         datePicker.setValue(UIColor.white, forKey: "textColor")
     }
-
+    
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        let formatter = DateFormatter()
+        
+        formatter.dateFormat = "HH:mm:ss"
+        
+        let settingTime = formatter.string(from: datePickerView.date)
+        
+        alarmTime = formatter.date(from: settingTime)
+    }
+    
+    @IBAction func btnStartAction(_ sender: UIButton) {
+        if(btnStartFlag == true){
+            Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+            
+            datePicker.isHidden = true
+            lblRemainTime.isHidden = false
+            
+            startButton.setTitle("그만", for: .normal)
+            btnStartFlag = false
+        }
+        else {
+            datePicker.isHidden = false
+            lblRemainTime.isHidden = true
+            btnStartFlag = true
+            startButton.setTitle("시작", for: .normal)
+        }
+    }
+    
+    @objc func updateTime() {
+        let formatter = DateFormatter()
+        let date = Date()
+        
+        formatter.dateFormat = "HH:mm:ss"
+        
+        let nowTime = formatter.string(from: date as Date)
+        let currentTime = formatter.date(from: nowTime)!
+        
+        var diff = Int(alarmTime?.timeIntervalSince(currentTime) ?? 0)
+        
+        var Time = ""
+        var sec = 0
+        var min = 0
+        var hour = 0
+        
+        if(diff < 0) {
+            diff = diff + 86400
+        }
+        
+        sec = diff%60
+        diff = diff/60
+        
+        min = diff%60
+        diff = diff/60
+        
+        hour = diff
+        
+        
+        if(hour < 10) {
+            Time = "0" + String(hour)
+        }
+        else{
+            Time = String(hour)
+        }
+        Time = Time + " : "
+        
+        if(min < 10) {
+            Time = Time + "0" + String(min)
+        }
+        else{
+            Time = Time + String(min)
+        }
+        Time = Time + " : "
+        
+        if(sec < 10) {
+            Time = Time + "0" + String(sec)
+        }
+        else {
+            Time = Time + String(sec)
+        }
+        
+        lblRemainTime.text = Time
+        lblRemainTime.textColor = UIColor.white
+        lblRemainTime.font = UIFont.systemFont(ofSize: CGFloat(50))
+        
+    }
 }
